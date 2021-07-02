c441c9f18224a4cb08560dd43f757121c3939d44
harshnion <86431354+harshnion@users.noreply.github.com>
2021-07-02 23:48:31 +0530

    9_commit

---
+++ Dictionary.py
@@ -0,0 +1,23 @@
+# Hello guys today we mill learn about another method of storing data 
+# i. e. Dictionary
+# in this we can make table 
+# just like matrix using list 
+
+# So Guys are you ready for 1st example
+movie = {
+  "name" : "Rocket singh",
+  "Year" : 2010,
+  "Hero" : "ranbir kapoor" 
+  }
+
+#Here we print our data
+print(movie)
+
+#Here we access a particular value of a key
+print(movie["Year"])
+
+# Here we add something extra to the list
+movie['rating'] = 9.2
+
+print(movie)
+#so guys in this way you learn something about dictionary
